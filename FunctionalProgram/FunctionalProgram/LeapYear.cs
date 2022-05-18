ce6181ae4cee2890c3d7fb20e762ee27ea0541fd
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-18 22:13:32 +0530

    [Suraj]: program to check leap year is added.

---
+++ FunctionalProgram/FunctionalProgram/LeapYear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgram
+{
+    internal class LeapYear
+    {
+        public void Year()
+        {
+            Console.WriteLine("Enter Year: ");
+            int Year = Convert.ToInt32(Console.ReadLine());
+            if ((Year % 4 == 0) && (Year % 100 != 0))
+            {
+                Console.WriteLine(Year + " is a Leap Year.");
+            }
+            else if (Year % 400 == 0)
+            {
+                Console.WriteLine(Year + " is a Leap Year.");
+
+            }
+            else
+            {
+                Console.WriteLine(Year + " is not a Leap Year.");
+            }
+        }
+    }
+}
--- FunctionalProgram/FunctionalProgram/Program.cs
+++ FunctionalProgram/FunctionalProgram/Program.cs
@@ -1,2 +1,15 @@
-﻿FunctionalProgram.Function function = new FunctionalProgram.Function();
-function.flipCoin();
+﻿Console.WriteLine("1.Flip Coin\n2.Leap Year\n");
+Console.WriteLine("Enter Your Choice:");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+switch (choice)
+{
+    case 1:
+        FunctionalProgram.Function function = new FunctionalProgram.Function();
+        function.flipCoin();
+        break;
+    case 2:
+        FunctionalProgram.LeapYear year = new FunctionalProgram.LeapYear();
+        year.Year();
+        break;
+}
