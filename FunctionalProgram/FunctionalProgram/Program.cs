d3c349dc12d59d758b6828d097a1f98bdb8f1337
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-19 09:35:56 +0530

    [Suraj]: Program to swap two nos. is added.

--- FunctionalProgram/FunctionalProgram/Program.cs
+++ FunctionalProgram/FunctionalProgram/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power of Two\n4.Harmonic Number\n5.Prime Factor\n6.Dividatio\n");
+﻿Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power of Two\n4.Harmonic Number\n5.Prime Factor\n6.Dividatio\n7.Swapping\n");
 Console.WriteLine("Enter Your Choice:");
 int choice = Convert.ToInt32(Console.ReadLine());
 
@@ -27,5 +27,9 @@ switch (choice)
     case 6:
         FunctionalProgram.ReminderQuotient cal = new FunctionalProgram.ReminderQuotient();
         cal.calculate();
+        break;
+    case 7:
+        FunctionalProgram.Swap swap = new FunctionalProgram.Swap();
+        swap.change();
         break;
 }
---
+++ FunctionalProgram/FunctionalProgram/Swap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgram
+{
+    internal class Swap
+    {
+        public void change()
+        {
+            Console.WriteLine("Enter first no.: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second no.: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before swapping A: " + a + " B is: " + b);
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After swapping A is: "+ a +" B is: "+ b);
+        }
+    }
+}
